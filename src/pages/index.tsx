8ceb1e7825e965f1ad2b0360d8611b8c2e22aea4
NWylynko <nick@wylynko.com>
2021-09-06 06:39:59 +0000

    center text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import React from "react"
+import styled from "styled-components"
 
 export default function IndexPage() {
   return (
-    <>
+    <Container>
       <h1>Hello Mr Nolan</h1>
-    </>
+    </Container>
   )
 }
+
+const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 98vh;
+`;
