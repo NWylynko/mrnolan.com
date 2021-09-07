0ff96a590425a84f854d4808b30245eff6c13187
NWylynko <nick@wylynko.com>
2021-09-07 02:11:31 +0000

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `MrNolan.com`,
+        short_name: `MrNolan`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/images/mrnolan-favicon.png`
+      },
+    },
+  ],
 }
